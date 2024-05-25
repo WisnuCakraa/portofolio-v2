8c6fdb4391aa2a78a4eb83bcd41ca8e790a7547b
wisnu cakra <wisnucakraa@gmail.com>
2024-05-25 17:57:26 +0700

    [CREATE] - Use Context for DarkMode

---
+++ src/context/DarkModeContext.tsx
@@ -0,0 +1,40 @@
+'use client';
+import React from 'react';
+import { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+
+interface DarkModeContextProps {
+  darkMode: boolean;
+  toggleDarkMode: () => void;
+}
+
+const DarkModeContext = createContext<DarkModeContextProps | undefined>(undefined);
+
+export const DarkModeProvider = ({ children }: { children: ReactNode }) => {
+  const [darkMode, setDarkMode] = useState(true);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
+
+export const useDarkMode = () => {
+  const context = useContext(DarkModeContext);
+  if (!context) {
+    throw new Error('useDarkMode must be used within a DarkModeProvider');
+  }
+  return context;
+};
